0bd2d6509b5ca3184422f8c3a675218961684391
NURUL EKA MARYANA <110007326+Nurul-ekamaryana@users.noreply.github.com>
2022-09-16 20:02:36 +0700

    Update input-datadiri.php

--- input-datadiri.php
+++ input-datadiri.php
@@ -1,48 +1,49 @@
-<form action="input-siswa.php" method="POST">
-    <label for="nis">Nomor induk siswa :</label>
-    <input type="number" name="nis" placeholder="Ex. 12001142"/><br>
+<form action="input-datadiri.php" method="POST">
+    <label for="nis">Nomor Induk Siswa:</label>
+    <input type="number" name="nis" placeholder="Ex. 120033102" /><br>
 
-    <label for="name">Nama lengkap :</label>
-    <input type="text" name="name" placeholder="Ex. Sehun oh"/><br>
+    <label for="nama">Nama Lengkap :</label>
+    <input type="text" name="nama" placeholder="Ex. Muthia" /><br>
 
-    <label for="Tanggal_lahir">Tanggal lahir :</label>
-    <input type="date" name="Tanggal_lahir" /><br>
+    <label for="tanggal_lahir">Tanggal Lahir :</label>
+    <input type="date" name="tanggal_lahir" placeholder="Ex. 120033102" /><br>
 
     <label for="nilai">Nilai :</label>
-    <input type="number" name="nilai" placeholder="Ex. 80.56"/><br>
+    <input type="number" name="nilai" placeholder="Ex. 80.56" /><br>
 
-    <input type="submit" name="simpan" value="simpan data" />
-   
-</form>
+    <input type="submit" name="simpan" value="Simpan Data" />
+</form>    
 
 <?php
-       include('./input-config.php');
-       echo "<hr>";
-       //menampilkan data diri databases
-       $no = 1;
-       $tabledata = "";
-       $data = mysqli_query($mysqli," SELECT * FROM datadiri");
-       while($row = mysqli_fetch_array($data)){
+    include('./input-config.php');
+    echo "<hr>";
+    // Menampilkan data dari database
+    $no = 1;
+    $tabledata = "";
+    $data = mysqli_query($mysqli," SELECT * FROM datadiri ");
+    while($row = mysqli_fetch_array($data)){
         $tabledata .= "
-        <tr> 
-             <td>".$row["nis"]."</td>
-             <td>".$row["namalengkap"]."</td>
-             <td>".$row["tanggal_lahir"]."</td>
-             <td>".$row["nilai"]."</td>
-        </tr>
-        ";
-        $no++;
-       }
+        <tr>
+            <td>".$row["nis"]. "</td>
+            <td>".$row["namalengkap"]. "</td>
+            <td>".$row["tanggal_lahir"]. "</td>
+            <td>".$row["nilai"]. "</td>
+        </tr> 
+             
+      ";
+      $no++;
+    }
 
-       echo "
-       <table callpadding=5 border=1 cellspacing=0>
-          <tr>
-             <th>NIS</th>
-             <th>Nama lengkap</th>
-             <th>tanggal lahir</th>
-             <th>Nilai</th>
-          </tr>
-          $tabledata
-       </table>
-        ";
-?>
+    echo "
+        <table cellpadding=5 border=1 cellspacing=0>
+        <tr>
+            <th>NIS</th>
+            <th>Nama Lengkap</th>
+            <th>Tanggal Lahir</th>
+            <th>Nilai</th>
+        </tr>
+        $tabledata
+     </table>       
+ ";
+
+ ?>
